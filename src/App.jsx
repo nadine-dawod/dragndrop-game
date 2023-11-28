31dc3af5fd6197432f621ad01bfab894d2a5be1d
Vardagsbruket <134447488+Vardagsbruket@users.noreply.github.com>
2023-11-28 19:37:01 +0100

    Adjust routes + add header and footer i App (#11)
    * move routes to separate folder + add header and footer
    
    * remove borders on header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
 import "./App.css";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import { HomePage } from "./pages/HomePage";
-import { ErrorLogin } from "./components/ErrorLogin";
-import { GamePage } from "./pages/GamePage";
-import { ProfilePage } from "./pages/ProfilePage";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
+import routes from "./routes/routes";
 
 function App() {
   return (
     <>
       <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="*" element={<ErrorLogin />} />
-          <Route path="/game/:userId" element={<GamePage />} />
-          <Route path="/profile/:userId" element={<ProfilePage />} />
-        </Routes>
+        <Header />
+        {routes}
+        <Footer />
       </Router>
     </>
   );
