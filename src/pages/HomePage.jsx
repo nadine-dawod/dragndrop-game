3ebc9cbee9785f901e6cf256e6f9b93db02b5c75
Nadine Dawod <127306010+nadine-dawod@users.noreply.github.com>
2023-12-04 10:14:23 +0100

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,5 @@
-<<<<<<< HEAD
 
 
 export const HomePage = () => {
   return <div></div>;
-=======
-import { GamePage } from "./GamePage";
-
-export const HomePage = () => {
-  return (
-    <div>
-      <h1>This is our game-app!</h1>
-      <GamePage />
-    </div>
-  );
->>>>>>> 62ce906233312919945679a5bff19b24d988eb88
 };
