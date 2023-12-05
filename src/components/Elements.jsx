3c2d2fb40d1ace49c284ab082e31007c3e2d41e0
SaskiaVdZ <143086816+SaskiaVdZ@users.noreply.github.com>
2023-12-05 18:11:28 +0100

    added component building elements and css instead of svg's directly in GamePage

---
+++ src/components/Elements.jsx
@@ -0,0 +1,24 @@
+import { Elementone } from "../components/Elements/Elementone";
+import { Elementtwo } from "../components/Elements/Elementtwo";
+import { Elementthree } from "../components/Elements/Elementthree";
+import { Elementfour } from "../components/Elements/Elementfour";
+import { Elementfive } from "../components/Elements/Elementfive";
+import { Elementsix } from "../components/Elements/Elementsix";
+import { Elementseven } from "../components/Elements/Elementseven";
+import "./Elements.css";
+
+
+
+export const Elements = () => {
+    return (
+      <div className="elements">
+         <Elementone/>
+         <Elementtwo/>
+         <Elementthree/>
+         <Elementfour/>
+         <Elementfive/>
+         <Elementsix/>
+         <Elementseven/>
+      </div>
+    );
+  };
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,6 @@ import { GamePage } from "./GamePage";
 export const HomePage = () => {
   return (
     <div>
-      <h1>This is our game-app!</h1>
       <GamePage />
     </div>
   );
