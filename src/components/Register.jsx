9d2060e170123af1eb19399e23a29993f10dc8c5
Maria Kim <143033862+MariaKim1@users.noreply.github.com>
2023-12-06 12:47:33 +0100

    Conflicts solved after merge - json installed, removed card, added button + Registerpage css (#38)
    * test commit
    
    * marias branch
    
    * breaking it down
    
    * change branch save
    
    * addded code for register account
    
    * added registeraccount-page, tried to view the login and register page - no success
    
    * minor changes commented out Card on Login
    
    * commented out Card on Login
    
    * save
    
    * added Card component
    
    * my changes
    
    * added Card + styling
    
    * added button styling
    
    * added styling for button and registeraccountpage
    
    * installed json package, adjusted handleSubmit function in Register accordingly
    
    * removed card.jsx + css and from Register

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,8 +1,9 @@
 // ==== REGISTER ACCOUNT ====
 import { useState } from "react";
-//import axios from "axios";
-//import Card from "./Card";
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { Button } from "./Button";
+import "./Register.css";
 // +++++ Added const navigate on this page after seeing the Login tutorial.
 // Imported useNavigate to RegisterAccount
 // Also imported RegisterAccount and added Route to app.jsx.
@@ -19,7 +20,7 @@ const Register = () => { // Three states - one for each input field
   };
 
   const handleSubmit = async () => { // Assign this to button
-    const user = await axios
+   /*  const user = await axios
       .get("/users")
       .then((res) => checkEmail(res.data, email));
 
@@ -30,13 +31,22 @@ const Register = () => { // Three states - one for each input field
       const user = { username, email, password };
       axios.post("/users", user).then(alert("User created!"));
       navigate("../ProfilePage");
-    }
+    } */
+    const user = { username, email, password };
+    axios
+        .post("http://localhost:6001/users", user)
+        .then(alert("User created!"));
+
+      setUser({
+        name: "",
+        email: "",
+        password: "",
+      });
   };
 
  
   return (
     <div className="container">
-      {/* <Card> */}
       <form className="form-container">
         <h1>Register User</h1>
         <label>
@@ -67,48 +77,8 @@ const Register = () => { // Three states - one for each input field
           <p>Register Account</p>
         </button> 
       </form>
-      {/* </Card> */} 
     </div>
   );
 };
 
 export default Register;
-
-// ==== My train of thought bellow (befor starting to code) ====
-
-// This will be the Create Account page.
-// It will include:
-// - a picture to the left
-// - a logo on the top/center
-
-// ==== FUNCTION TO CREATE ACCOUNT ====
-
-// HELENA:
-// Spara personen ngnstans - json server.
-// -- Specifika sätt för register samt login. (Group project resourses)
-// -- Koden ska finnas på github - sundsgården (all source code).
-// Registrera funktionen.
-// Funkar Login?
-
-// Store "create acount" data in an object.
-// The stored account data should be able to be displayed on the Profile page.
-
-// - you should be able to choose one out of two game-plan options - free or pro.
-// 1. make 2 checkboxes together with some text.
-// 2. create a variable that will store the choice of which checkbox was selected.
-// 3. if/else statement to navigate to pro or free version ((extra feature))
-
-// ---- THE LOGIN feature ----
-// - three fields to fill in create account info: Username, Email, password
-// - all three fields should be stored in ONE user-object.
-// 1. Create a field for USERNAME.
-// 2. Create a variable? to store the info connected to this userinfo.
-// 3. Error message: How to tell the username is available?
-//
-// 1. create a field for EMAIL
-// 2. store this info in the same object as the username.
-// 3. Error message: requier an @ = valid email? Is there a standard code snippet for this?
-//
-// 1. create a field for PASSWORD
-// 2. store the PW in the same object as the username and email.
-// 3. Error message: requirements? No requirements.
