d60e9e036e1c8cd51a98d93de799fdb62476ac70
SaskiaVdZ <143086816+SaskiaVdZ@users.noreply.github.com>
2023-12-06 09:18:33 +0100

    Profile Picture in header

--- src/components/ProfilePicture.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-function ProfilePicture() {
-    return (
-        <div><img src={require('.components/Images/ProfilePhoto.png')} alt="Profile Picture"></img></div>
-    )
-}
-export default ProfilePicture;
