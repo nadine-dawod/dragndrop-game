13d185d692974d26c4d8cc24d2e4a4a45eefac32
Maria Kim <143033862+MariaKim1@users.noreply.github.com>
2023-12-11 10:57:58 +0100

    add working register account code + PW validation + email/username validation (#46)
    * test commit
    
    * marias branch
    
    * breaking it down
    
    * change branch save
    
    * addded code for register account
    
    * added registeraccount-page, tried to view the login and register page - no success
    
    * minor changes commented out Card on Login
    
    * commented out Card on Login
    
    * save
    
    * added Card component
    
    * my changes
    
    * added Card + styling
    
    * added button styling
    
    * added styling for button and registeraccountpage
    
    * installed json package, adjusted handleSubmit function in Register accordingly
    
    * removed card.jsx + css and from Register
    
    * added pw-validation (not done), had to revert back to old commit
    
    * added and updated register code - pw + user validation
    
    * cleaned Register - code DONE. Checked w Login seems to work.
    
    * solve conflct in register

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -4,46 +4,49 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { Button } from "./Button";
 import "./Register.css";
-// +++++ Added const navigate on this page after seeing the Login tutorial.
-// Imported useNavigate to RegisterAccount
-// Also imported RegisterAccount and added Route to app.jsx.
 
-const Register = () => { // Three states - one for each input field 
+//  TODO:
+//  1. does the register function work with t he login function? I THINK SO, BUT CHECK AGAIN AFTER MERGE
+//  2. Add correct navigation iin register code.
+  
+
+const Register = () => { 
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [password1, setPassword1] = useState("");
+  const [password2, setPassword2] = useState("");
   const navigate = useNavigate();
 
-  const checkEmail = (users) => { // check if the email is unique
-    const user = users.find((user) => user.email === email);
-    if (user) return user;
-  };
 
-  const handleSubmit = async () => { // Assign this to button
-   /*  const user = await axios
-      .get("/users")
-      .then((res) => checkEmail(res.data, email));
+  const handleSubmit = async (e) => {   // Assign this function to button onClick
+    e.preventDefault();
 
-    
-    if (user) { // if email already exists
-      alert("User already exists!");
-    } else {  // else save this new user - by making a post-request
-      const user = { username, email, password };
-      axios.post("/users", user).then(alert("User created!"));
-      navigate("../ProfilePage");
-    } */
-    const user = { username, email, password };
-    axios
-        .post("http://localhost:6001/users", user)
-        .then(alert("User created!"));
+        if (password1 === password2) {
+        
+          try {
+          const response = await axios.get("http://localhost:6001/users"); 
+            const users = response.data     //list of users (json) = response.data
+            const user = users.find((user) => user.email === email | user.username === username); // check if email and/or username already exists
+            
+          if (user) {       // if - there is a matching user alert
+            alert("Username or email already exists. Please try again!");
+          } else {          // else - create newUser
+            console.log("i else satsen")
+            const newUser = { username, email, password: password1 };
+              axios.post("http://localhost:6001/users", newUser)  
+              .then(() => alert("User created!"));                
+              navigate("/"); // ==== ADD CORRECT ROUTE HERE ===  
+          } 
 
-      setUser({
-        name: "",
-        email: "",
-        password: "",
-      });
-  };
-
+          }
+          catch (error) {
+            console.log(error);
+          }
+        }
+        else { 
+          alert ("Passwords did not match. Please try again.");
+        }
+  }
  
   return (
     <div className="container">
@@ -52,9 +55,10 @@ const Register = () => { // Three states - one for each input field
         <label>
           <input
             type="text"
-            placeholder="Name"
-            value={username}  // added value to each state
-            onChange={(e) => setUsername(e.target.value)} // added onChange to each state to be able to update the state (from input field)
+            placeholder="Username"
+            value={username}  
+            onChange={(e) => setUsername(e.target.value)} // onChange to each state to be able to update the state (from input field)
+            required
           />
         </label>
         <label>
@@ -63,22 +67,33 @@ const Register = () => { // Three states - one for each input field
             placeholder="Email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+            required
           />
         </label>
         <label>
           <input
             type="password"
             placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            value={password1}
+            required
+            onChange={(e) => setPassword1(e.target.value)}
           />
-        </label> 
+        </label>
+        <label>
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            value={password2}
+            required
+            onChange={(e) => setPassword2(e.target.value)}
+          />
+        </label>
         <button className="btn" type="submit" onClick={handleSubmit}>
           <p>Register Account</p>
         </button> 
       </form>
     </div>
-  );
+  )
 };
 
 export default Register;
