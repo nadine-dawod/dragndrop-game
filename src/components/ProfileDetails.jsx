d86a82c94eedf696ebfd6d285a7b8b923985e48e
SaskiaVdZ <143086816+SaskiaVdZ@users.noreply.github.com>
2023-12-12 11:06:44 +0100

    Profile page, add function to component profile details, errorhandling and return userlist function (#51)
    * solve conflicts json package
    
    * add first part for  profileDetails and error handling
    
    * add return user list function in profileDetails component

--- src/components/ProfileDetails.jsx
+++ src/components/ProfileDetails.jsx
@@ -1,3 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 export const ProfileDetails = () => {
-  return <div></div>;
+  const [users, setUsers] = useState ([]);
+  useEffect(() => {
+    axios.get ('json server url')
+    .then(response=> {
+      setUsers(response.data);
+    })
+    .catch(error => { console.error('Oops, there is a problem fetching data:', error);
+  });
+  }, []);
+
+  return (
+  <div>
+    <h1>User List</h1>
+    <ul>
+      {users.map(user => (
+        <li key={user.id}>
+          <strong>Username:</strong> {user.username}<br />
+          <strong>Email:</strong> {user.email}
+        </li>
+      ))}
+    </ul>
+  </div>
+  );
 };
