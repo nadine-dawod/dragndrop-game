dd5b6d05cbde76537032a43a55cbf73e4890b8a9
Nadine Dawod <127306010+nadine-dawod@users.noreply.github.com>
2023-12-13 14:41:17 +0100

    moving id from svg element to div element

--- src/components/Elements/Elementone.jsx
+++ src/components/Elements/Elementone.jsx
@@ -17,8 +17,8 @@ export const Elementone = () => {
     } : undefined;
 
     return (
-    <div ref={NrOne} style={style} {...attribute} {...listeners} {...props}> 
-        <svg id="One" width="100" height="140" viewBox="0 0 100 140" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <div id="One" ref={NrOne} style={style} {...attribute} {...listeners} {...props}> 
+        <svg width="100" height="140" viewBox="0 0 100 140" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path fillRule="evenodd" clipRule="evenodd" d="M100 0H0V140H100V0ZM24.9999 43.9998L25 44.0695V140H75V43.9995H74.9999C74.9997 30.7448 63.8069 19.9998 49.9999 19.9998C36.1927 19.9998 24.9999 30.7449 24.9999 43.9998Z" fill="#EACFC4"/>
         </svg>       
     </div>     
