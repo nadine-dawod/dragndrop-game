399fa83efd2eebc2f34a8834f2310a39d99cf2e6
Diana Undén <134447488+Vardagsbruket@users.noreply.github.com>
2024-01-03 16:29:19 +0100

    Revert "add private routes and alert that user needs to log in to access private route (#70)" (#71)
    This reverts commit 7430f353553c3a53eff9ebf36f84f8fd8ac51aaf.

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,38 +1,21 @@
 import { Route, Routes } from "react-router-dom";
+import { HomePage } from "../pages/HomePage";
 import GamePage from "../pages/GamePage";
 import { ProfilePage } from "../pages/ProfilePage";
 import { AboutPage } from "../pages/AboutPage";
 import { LoginAccountPage } from "../pages/LoginAccountPage";
 import { RegisterAccountPage } from "../pages/RegisterAccountPage";
 import ErrorLogin from "../components/ErrorLogin";
-import ProtectedRoute from "./protectedRoutes";
-import { ProfileDetails } from "../components/profile/ProfileDetails";
+import Login from "../components/login/Login";
 
 const routes = (
   <Routes>
     <Route path="/" element={<LoginAccountPage />} />
+    <Route path="/game/:userId" element={<GamePage />} />
+    <Route path="/profile/:userId" element={<ProfilePage />} />
     <Route path="/about" element={<AboutPage />} />
     <Route path="/login" element={<LoginAccountPage />} />
     <Route path="/register-account" element={<RegisterAccountPage />} />
-    {/* Private routes */}
-
-    <Route
-      path="/game/:userId"
-      element={
-        <ProtectedRoute>
-          <GamePage />
-        </ProtectedRoute>
-      }
-    />
-    <Route
-      path="/profile/:userId"
-      element={
-        <ProtectedRoute>
-          <ProfilePage />
-        </ProtectedRoute>
-      }
-    />
-    {/* Error page */}
     <Route path="*" element={<ErrorLogin />} />
   </Routes>
 );
