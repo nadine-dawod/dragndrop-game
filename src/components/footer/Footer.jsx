1b21125c0bdebfd894e099c10f9ef74df0780b82
Nadine Dawod <127306010+nadine-dawod@users.noreply.github.com>
2024-01-03 18:54:17 +0100

    adding github links to names in footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,8 +4,7 @@ import { Link } from 'react-router-dom'
 export const Footer = () => {
   return (
     <div className="footer">
-      <p>Made with ❤️ in Sweden by Maria Persson + Mo Wei Wu + 
-        Nadine Dawod + Diana Undén + Saskia van der Zanden<Link to="https://github.com/Vardagsbruket/sundsgarden_game_dragndrop/tree/main"> - GitHub</Link></p>
+      <p>Made with ❤️ in Sweden by <a className="gh-link" href="https://github.com/MariaKim1">Maria Kim</a> + <a className="gh-link" href="https://github.com/momowo2023">Mo Wei Wu</a> + <a className="gh-link" href="https://github.com/nadine-dawod">Nadine Dawod</a> + <a className="gh-link" href="https://github.com/Vardagsbruket">Diana Undén</a> + <a className="gh-link" href="https://github.com/SaskiaVdZ">Saskia van der Zanden</a></p>
        
     </div>
   );
