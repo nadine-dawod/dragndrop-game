00a01b1fd8885ba28044b0d475a653ba8b7e8304
Maria Kim <143033862+MariaKim1@users.noreply.github.com>
2024-01-05 16:38:03 +0100

    Clean up commented out code in register.css + login.css. Added comments in register.jsx (#93)
    * test commit
    
    * commented out Card on Login
    
    * debug to get npm run dev to work - install @use-gesture/react and remove @rollup/rollup-darwin-x64 from dependencies in package-lock.json n package.json
    
    * RegisterAccountPage - add img + components according to Figma sketch (register options, radio buttons). Add styling for h1 in Register.css
    
    * add styling to register.css main text + radio buttons
    
    * small cleanup in Register.jsx and add more styling to Register.css - button left to style.
    
    * deleated comments and old code in register.jsx and register.css
    
    * removed width-styling in Register.css .register-form
    
    * add a todo-list and registered new user
    
    * add styling for register-btn + update text for newUser alert
    
    * add fontawesom script t index.html + green heart in footer. add new styling to .page-container
    
    * delete unused icon of green heart
    
    * AboutPage.css added and done. AboutPage.jsx: removed inline styling
    
    * Delete an extra main.jsx. Update and add text and placement styling to Login.css. Import btn styling to Rregister.jsx. Grammar-check in AboutPage.jsx. Sync classNames for radio buttons RegisterAccountPage.jsx.
    
    * removed todo-list + modified catch(error) in Register.jsx
    
    * clean up commented out code in Register.css + Login.css. Remove class .btn-p in Login.css bcs couldnt find in the html/jsx. Add comments to Register.jsx

--- src/components/registerAccount/Register.jsx
+++ src/components/registerAccount/Register.jsx
@@ -1,65 +1,70 @@
-// ==== REGISTER ACCOUNT ====
+// ==== REGISTER ACCOUNT / CREATE ACCOUNT ====
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import "./Register.css";
+import "../Button.css";
+  
 
-
-
-
-//  TODO:
-//  1. Add correct navigation to profile page after register - private route.
-//  2. add error page - add anything to catch (error)?
-
-const Register = () => {
+const Register = () => {  
+  // State variables to hold the form info
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password1, setPassword1] = useState("");
   const [password2, setPassword2] = useState("");
-  const navigate = useNavigate();
+  const navigate = useNavigate();  
+  // Hook to navigate to different pages
 
-  const handleSubmit = async (e) => {
-    // Assign this function to button onClick
+
+  const handleSubmit = async (e) => {   
+    // Function to handle form submission. Assign this function to button onClick
     e.preventDefault();
 
-    if (password1 === password2) {
-      try {
-        const response = await axios.get("http://localhost:6001/users");
-        const users = response.data; //list of users (json) = response.data
-        const user = users.find(
-          (user) => (user.email === email) | (user.username === username)
-        ); // check if email and/or username already exists
-
-        if (user) {
-          // if - there is a matching user alert
-          alert("Username or email already exists. Please try again!");
-        } else {
-          // else - create newUser
-          const newUser = { username, email, password: password1 };
-
-          axios
-            //posting/saving the new user and details to the json-server
-            .post("http://localhost:6001/users", newUser)
-            .then(() => alert("User created! Login to play!"));
-          navigate("/");
+        if (password1 === password2) {  
+          // Check if passwords match
+        
+          try { 
+            // Grab existing users from the server
+          const response = await axios.get("http://localhost:6001/users"); 
+            const users = response.data     
+            // List of users (json) = response.data
+            const user = users.find((user) => user.email === email | user.username === username); 
+            // Check if email and/or username already exists 
+            
+          if (user) {       
+            // If there is a matching user, alert..
+            alert("Username or email already exists. Please try again!");
+          } else {          
+            // Or else create newUser and display success alert..
+            const newUser = { username, email, password: password1 };
+              axios.post("http://localhost:6001/users", newUser)  
+              .then(() => alert("User created! Login to play!"));                
+              navigate("/");  
+              // Navigate to the Landing/Login-page after successful registration
+          } 
+          }
+          catch (error) {  
+            // Handle possible errors during the API request - message + error placeholder
+            console.error("Oops, something went wrong:", error);
+          }
         }
-      } catch (error) {
-        console.log(error);
-      }
-    } else {
-      alert("Passwords did not match. Please try again.");
-    }
+        else {   
+          // Or if passwords don't match, alert..
+          alert ("Passwords did not match. Please try again.");
+        }
   };
-
-  return (
+ 
+  return (  
+    // Form with input fields for username, email, password, confirm password, and button to submitt form
     <div className="container">
       <form className="form-container">
-        <label>
+        <label>  
           <input
             type="text"
             placeholder="Username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)} // onChange to each state to be able to update the state (from input field)
+            value={username}  
+            onChange={(e) => setUsername(e.target.value)}  
+            // onChange added to each state to be able to update the state (from input field)
             required
           />
         </label>
