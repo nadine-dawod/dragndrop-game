e491aa08df49ddbce012c8570bc81fc3d3c0330f
Nadine <nadinekdawod@gmail.com>
2024-01-05 11:11:43 +0100

    making links in footer open in another window

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 export const Footer = () => {
   return (
     <div className="footer">
-      <p>Made with <i class="fa-solid fa-heart" style={{color: "#9ce5ca"}}></i> in Sweden by <a className="gh-link" href="https://github.com/MariaKim1">Maria Kim</a> + <a className="gh-link" href="https://github.com/momowo2023">Mo Wei Wu</a> + <a className="gh-link" href="https://github.com/nadine-dawod">Nadine Dawod</a> + <a className="gh-link" href="https://github.com/Vardagsbruket">Diana Undén</a> + <a className="gh-link" href="https://github.com/SaskiaVdZ">Saskia van der Zanden</a></p>
+      <p>Made with <i class="fa-solid fa-heart" style={{color: "#9ce5ca"}}></i> in Sweden by <a className="gh-link" href="https://github.com/MariaKim1" target="_blank">Maria Kim</a> + <a className="gh-link" href="https://github.com/momowo2023" target="_blank">Mo Wei Wu</a> + <a className="gh-link" href="https://github.com/nadine-dawod" target="_blank">Nadine Dawod</a> + <a className="gh-link" href="https://github.com/Vardagsbruket" target="_blank">Diana Undén</a> + <a className="gh-link" href="https://github.com/SaskiaVdZ" target="_blank">Saskia van der Zanden</a></p>
        
     </div>
   );
